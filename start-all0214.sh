750e4ab7f7bc613db2f2d1bcf613cd56921b7745
Brent Groves <brent.groves@gmail.com>
2020-02-14 15:45:00 -0500

    Added/Changed Code

---
+++ start-all0214.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+( cd ~/srcnode/kors/bpg-services && npm start & cd ~/srcnode/kors/React13318 && npm start & node ~/srcnode/kors/MySql13318/app.js & node ~/srcnode/kors/Plex13318/app.js & node ~/srcnode/kors/Sproc13318/app.js & node ~/srcnode/kors/Alarms13318/app.js & node ~/srcnode/kors/Kep13318/app.js )
